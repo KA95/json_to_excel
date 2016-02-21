1910059dbfb6bc7eacb321b4351b08e468fdefc2
KA95 <antonkl@mail.ru>
2016-02-21 23:55:58 +0300

    Reading jsons.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
-/**
- * Created by Anton Klimansky.
- */
+import helper.JSONHelper;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    public static void main(String[] args) throws IOException, JSONException {
+        JSONArray jsonArray = JSONHelper.readJsonArrayFromUrl("http://export.zoomos.by/api/categories?key=api-help");
+        System.out.println("hello world");
     }
 }
---
+++ src/main/java/helper/JSONHelper.java
@@ -0,0 +1,39 @@
+package helper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+/**
+ * Created by Anton Klimansky.
+ */
+public class JSONHelper {
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonObjectFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        }
+    }
+
+    public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            return new JSONArray(jsonText);
+        }
+    }
+}
